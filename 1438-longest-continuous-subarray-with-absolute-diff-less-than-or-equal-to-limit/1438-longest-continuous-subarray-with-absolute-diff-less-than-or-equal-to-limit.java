fe98cbf5c3c0f0deb146525c849d28eac33a0bee
kamisetty leela lasya priya <kamisettylasya@gmail.com>
2026-04-01 10:35:55 +0530

    Time: 34 ms (58.96%), Space: 86.7 MB (88.31%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int longestSubarray(int[] A, int limit) {
+
+        Deque<Integer> maxd = new ArrayDeque<>();
+        Deque<Integer> mind = new ArrayDeque<>();
+
+        int i = 0, j;
+
+        for (j = 0; j < A.length; ++j) {
+
+            // maintain decreasing deque for max
+            while (!maxd.isEmpty() && A[j] > maxd.peekLast())
+                maxd.pollLast();
+
+            // maintain increasing deque for min
+            while (!mind.isEmpty() && A[j] < mind.peekLast())
+                mind.pollLast();
+
+            maxd.add(A[j]);
+            mind.add(A[j]);
+
+            // shrink window if invalid
+            if (maxd.peek() - mind.peek() > limit) {
+
+                if (maxd.peek() == A[i])
+                    maxd.poll();
+
+                if (mind.peek() == A[i])
+                    mind.poll();
+
+                i++;
+            }
+        }
+
+        return j - i;
+    }
+}
