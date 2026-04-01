a21db5b5d2361ef23a13ef53cd318160d812e836
kamisetty leela lasya priya <kamisettylasya@gmail.com>
2026-04-01 11:28:16 +0530

    Time: 6 ms (97.13%), Space: 47.6 MB (65.91%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,29 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        while (root != null) {
+
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } 
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } 
+            else {
+                return root; // LCA found
+            }
+        }
+
+        return null;
+    }
+}
